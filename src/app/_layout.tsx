946030ba6e9feb5bcc66208a14a6019db289f843
Jean Paul Rojas <eccianime@gmail.com>
2025-05-26 14:56:06 -0300

    feat: Add tailwind config

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,18 @@
+import '../../global.css';
+
+import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  const [fontsLoaded] = useFonts({
+    LaptureSemibold: require('../assets/fonts/Lapture-Semibold.otf'),
+  });
+
+  if (!fontsLoaded) return null;
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name='index' />
+      <Stack.Screen name='game' />
+    </Stack>
+  );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  presets: [require('nativewind/preset')],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          100: '#f9dac8',
+          200: '#585858',
+          300: '#ea3424',
+        },
+      },
+      fontFamily: {
+        LaptureSemiBold: 'Lapture-Semibold',
+      },
+    },
+  },
+  plugins: [],
+};
